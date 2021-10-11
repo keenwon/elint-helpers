a0890636ea3f821e4eccce460aa45cc608d42361
keenwon <semanwmj@gmail.com>
2021-10-11 11:47:35 +0800

    feat: 支持全部的配置文件格式

--- src/config.js
+++ src/config.js
@@ -4,17 +4,37 @@
 const linterConfigFile = [
   // eslint
   '.eslintrc.js',
+  '.eslintrc.cjs',
+  '.eslintrc.yaml',
+  '.eslintrc.yml',
+  '.eslintrc.json',
   '.eslintignore',
 
   // stylelint
   '.stylelintrc.js',
+  '.stylelintrc.cjs',
+  '.stylelintrc.yaml',
+  '.stylelintrc.yml',
+  '.stylelintrc.json',
+  'stylelint.config.js',
+  'stylelint.config.cjs',
   '.stylelintignore',
 
   // husky
   '.huskyrc.js',
+  '.huskyrc.yaml',
+  '.huskyrc.yml',
+  '.huskyrc.json',
 
   // commitlint
+  '.commitlintrc',
+  '.commitlintrc.json',
+  '.commitlintrc.yaml',
+  '.commitlintrc.yml',
+  '.commitlintrc.ts',
   '.commitlintrc.js',
+  '.commitlintrc.config.ts',
+  '.commitlintrc.config.js',
 
   // prettier
   '.prettierrc.js',
